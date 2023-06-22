def659d09778869992017a1b8041c12876bcdbf8
友瀬久美子 <tomose@tomoseMacBook-Pro.local>
2023-06-22 17:14:35 +0900

    refactor: StatefulWidgetパターン

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -40,6 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    print('MyHomePage build');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -49,21 +49,56 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const WidgetA(),
+            WidgetB(_counter),
+            WidgetC(_incrementCounter),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+}
+
+class WidgetA extends StatelessWidget {
+  const WidgetA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    print('WidgetA build');
+    return const Text('You have pushed the button this many times:');
+  }
+}
+
+class WidgetB extends StatelessWidget {
+  const WidgetB(this.counter, {super.key});
+  final int counter;
+
+  @override
+  Widget build(BuildContext context) {
+    print('WidgetB build');
+    return Column(
+      children: [
+        Text(
+          '$counter',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ],
+    );
+  }
+}
+
+class WidgetC extends StatelessWidget {
+  const WidgetC(this.increment, {super.key});
+  final Function increment;
+
+  @override
+  Widget build(BuildContext context) {
+    print('WidgetC build');
+    return ElevatedButton(
+      onPressed: () {
+        increment();
+      },
+      child: const Text('カウント'),
     );
   }
 }

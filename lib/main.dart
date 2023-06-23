6337104fa42425fb2af038eca09971a18c34c255
tms-itz <k-tomose@itsol.co.jp>
2023-06-23 15:13:21 +0900

    refactor: InheritedWidgetパターン

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -25,37 +25,76 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MyHomePageState extends State<MyHomePage> {
+  int counter = 0;
 
-  void _incrementCounter() {
+  void incrementCounter() {
     setState(() {
-      _counter++;
+      counter++;
     });
+  }
+
+  void rebuild() {
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     print('MyHomePage build');
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const WidgetA(),
-            WidgetB(_counter),
-            WidgetC(_incrementCounter),
-          ],
+    return MyHomePageInheritedWidget(
+      data: this,
+      counter: counter,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              WidgetA(),
+              WidgetB(),
+              WidgetC(),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class MyHomePageInheritedWidget extends InheritedWidget {
+  const MyHomePageInheritedWidget({
+    Key? key,
+    required Widget child,
+    required this.data,
+    required this.counter,
+  }) : super(key: key, child: child);
+
+  final MyHomePageState data;
+  final int counter;
+
+  static MyHomePageState of(BuildContext context, {bool listen = true}) {
+    if (listen) {
+      return (context
+              .dependOnInheritedWidgetOfExactType<MyHomePageInheritedWidget>()!)
+          .data;
+    } else {
+      return (context
+              .getElementForInheritedWidgetOfExactType<
+                  MyHomePageInheritedWidget>()!
+              .widget as MyHomePageInheritedWidget)
+          .data;
+    }
+  }
+
+  @override
+  bool updateShouldNotify(MyHomePageInheritedWidget oldWidget) {
+    return counter != oldWidget.counter; // カウンターの値が変わったら再描画
   }
 }
 
@@ -70,35 +109,44 @@ class WidgetA extends StatelessWidget {
 }
 
 class WidgetB extends StatelessWidget {
-  const WidgetB(this.counter, {super.key});
-  final int counter;
+  const WidgetB({super.key});
 
   @override
   Widget build(BuildContext context) {
     print('WidgetB build');
-    return Column(
-      children: [
-        Text(
-          '$counter',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-      ],
+    final MyHomePageState state = MyHomePageInheritedWidget.of(context);
+
+    return Text(
+      '${state.counter}',
+      style: Theme.of(context).textTheme.headlineMedium,
     );
   }
 }
 
 class WidgetC extends StatelessWidget {
-  const WidgetC(this.increment, {super.key});
-  final Function increment;
+  const WidgetC({super.key});
 
   @override
   Widget build(BuildContext context) {
     print('WidgetC build');
-    return ElevatedButton(
-      onPressed: () {
-        increment();
-      },
-      child: const Text('カウント'),
+    final MyHomePageState state =
+        MyHomePageInheritedWidget.of(context, listen: false); //falseにすると再描画されない
+
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            state.incrementCounter();
+          },
+          child: const Text('カウント'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            state.rebuild();
+          },
+          child: const Text('再描画'),
+        )
+      ],
     );
   }
 }
